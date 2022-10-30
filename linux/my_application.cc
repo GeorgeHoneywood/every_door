f3b5ad7baee4c0774c70ef92e5f63d3d96022c5f
GeorgeHoneywood <george.honeywood.2019@rhul.ac.uk>
2022-10-30 16:16:06 +0000

    fix: correct app name in window title

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "every_door");
+    gtk_header_bar_set_title(header_bar, "Every Door");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "every_door");
+    gtk_window_set_title(window, "Every Door");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
